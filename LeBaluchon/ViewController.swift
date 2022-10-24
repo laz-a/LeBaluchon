ab828a89a1b001da0b26b235c7acb7a2b129da12
laz <laz@laz.com>
2022-10-24 15:52:41 +0200

    Initial Commit

---
+++ LeBaluchon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeBaluchon
+//
+//  Created by laz on 24/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
